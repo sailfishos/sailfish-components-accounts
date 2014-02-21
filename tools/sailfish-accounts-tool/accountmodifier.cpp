d41ae0748dfebfc0caa14c00809e7f67a76510eb
Chris Adams <chris.adams@jollamobile.com>
2014-02-21 18:45:10 +1000

    [sailfish-accounts-tool] Make tool more robust. Contributes to JB#16285
    Previously, the account ptr was dereferenced without checking.
    It can be null if the accounts database is locked at the time
    that the tool is run.
    This commit also ensures that non-blocking database syncs are
    used to avoid possible i/o deadlocks.
    
    Contributes to JB#16285

--- tools/sailfish-accounts-tool/accountmodifier.cpp
+++ tools/sailfish-accounts-tool/accountmodifier.cpp
@@ -8,6 +8,19 @@
 
 #include <QtDebug>
 
+AccountModifier::AccountModifier(QObject *parent)
+    : QObject(parent)
+    , m_accountManager(new Accounts::Manager)
+    , m_currAccount(0)
+    , m_currAccountIdx(-1)
+{
+}
+
+AccountModifier::~AccountModifier()
+{
+    delete m_accountManager;
+}
+
 // helper to perform conversion from argument passed via commandline into QVariant for storage/setting.
 QVariant convertSettingValue(const QString &settingName, const QString &settingType, const QString &settingValue)
 {
@@ -35,6 +48,7 @@ QVariant convertSettingValue(const QString &settingName, const QString &settingT
         variantSettingValue = QVariant::fromValue<qulonglong>(ulonglongSettingValue);
     } else {
         // not a valid setting type, return invalid variant.
+        qWarning() << Q_FUNC_INFO << "invalid setting type specified for" << settingName;
     }
 
     return variantSettingValue;
@@ -42,8 +56,18 @@ QVariant convertSettingValue(const QString &settingName, const QString &settingT
 
 void AccountModifier::start()
 {
-    Accounts::Manager mgr;
-    Accounts::AccountIdList allAccounts = mgr.accountList();
+    if (!m_accountManager) {
+        qWarning() << Q_FUNC_INFO << "could not instantiate account manager!";
+        emit done();
+        return;
+    }
+
+    m_allAccountIds = m_accountManager->accountList();
+    if (m_allAccountIds.size() == 0) {
+        qWarning() << Q_FUNC_INFO << "no accounts to modify";
+        emit done();
+        return;
+    }
 
     bool emptyProvider = providerName.isEmpty();
     bool emptyService = serviceName.isEmpty();
@@ -70,36 +94,71 @@ void AccountModifier::start()
                    << serviceName << "of" << providerName << "accounts!";
     }
 
-    foreach (const Accounts::AccountId &accId, allAccounts) {
-        Accounts::Account *acc = mgr.account(accId);
-        if (providerName.isEmpty() || acc->providerName() == providerName) {
-            QVariant variantSettingValue = convertSettingValue(settingName, settingType, settingValue);
-            if (serviceName == QString::fromLatin1("--global")) {
-                // modify/remove the global service setting only
-                acc->selectService(Accounts::Service());
-                if (modeSwitch == QString::fromLatin1("-r")) {
-                    acc->remove(settingName);
-                } else {
-                    acc->setValue(settingName, variantSettingValue);
-                }
-            } else {
-                // modify/remove the setting for a particular service or for all services
-                Accounts::ServiceList allServices = acc->services();
-                foreach (const Accounts::Service &srv, allServices) {
-                    if (serviceName.isEmpty() || srv.name() == serviceName) {
-                        acc->selectService(srv);
-                        if (modeSwitch == QString::fromLatin1("-r")) {
-                            acc->remove(settingName);
-                        } else {
-                            acc->setValue(settingName, variantSettingValue);
-                        }
-                        acc->selectService(Accounts::Service());
-                    }
-                }
-            }
-            acc->syncAndBlock();
-        }
+    m_currAccountIdx = -1;
+    next();
+}
+
+void AccountModifier::next()
+{
+    if (m_currAccount) {
+        m_currAccount->disconnect(this);
     }
 
-    emit done();
+    m_currAccountIdx += 1;
+    if (m_currAccountIdx >= m_allAccountIds.size()) {
+        emit done();
+        return;
+    }
+
+    Accounts::AccountId currAccountId = m_allAccountIds.at(m_currAccountIdx);
+    m_currAccount = currAccountId > 0 ? m_accountManager->account(currAccountId) : 0;
+    if (!m_currAccount) {
+        qWarning() << Q_FUNC_INFO << "Unknown error: account could not be retrieved:" << currAccountId;
+        next();
+        return;
+    }
+
+    connect(m_currAccount, SIGNAL(error(Accounts::Error)), this, SLOT(error(Accounts::Error)));
+    connect(m_currAccount, SIGNAL(synced()), this, SLOT(next()));
+    bool doingSync = false;
+
+    if (providerName.isEmpty() || m_currAccount->providerName() == providerName) {
+        QVariant variantSettingValue = convertSettingValue(settingName, settingType, settingValue);
+        if (serviceName == QString::fromLatin1("--global")) {
+            // modify/remove the global service setting only
+            m_currAccount->selectService(Accounts::Service());
+            if (modeSwitch == QString::fromLatin1("-r")) {
+                m_currAccount->remove(settingName);
+            } else {
+                m_currAccount->setValue(settingName, variantSettingValue);
+            }
+        } else {
+            // modify/remove the setting for a particular service or for all services
+            Accounts::ServiceList allServices = m_currAccount->services();
+            foreach (const Accounts::Service &srv, allServices) {
+                if (serviceName.isEmpty() || srv.name() == serviceName) {
+                    m_currAccount->selectService(srv);
+                    if (modeSwitch == QString::fromLatin1("-r")) {
+                        m_currAccount->remove(settingName);
+                    } else {
+                        m_currAccount->setValue(settingName, variantSettingValue);
+                    }
+                    m_currAccount->selectService(Accounts::Service());
+                }
+            }
+        }
+        doingSync = true;
+        m_currAccount->sync(); // can't use syncAndBlock() or it causes deadlock in some cases.
+    }
+
+    if (!doingSync) {
+        next();
+    }
+}
+
+void AccountModifier::error(Accounts::Error err)
+{
+    qWarning() << Q_FUNC_INFO << "error during sync of account" << m_currAccount->id()
+               << ":" << err.type() << err.message();
+    next();
 }
--- tools/sailfish-accounts-tool/accountmodifier_p.h
+++ tools/sailfish-accounts-tool/accountmodifier_p.h
@@ -8,6 +8,10 @@
 #include <QObject>
 #include <QString>
 
+#include <Accounts/Error>
+#include <Accounts/Account>
+#include <Accounts/Manager>
+
 class AccountModifier : public QObject
 {
     Q_OBJECT
@@ -20,11 +24,22 @@ public:
     QString settingType;
     QString settingValue;
 
+    AccountModifier(QObject *parent = 0);
+    ~AccountModifier();
+
 public Q_SLOTS:
     void start();
+    void next();
+    void error(Accounts::Error err);
 
 Q_SIGNALS:
     void done();
+
+private:
+    Accounts::Manager *m_accountManager;
+    Accounts::AccountIdList m_allAccountIds;
+    Accounts::Account *m_currAccount;
+    int m_currAccountIdx;
 };
 
 #endif
