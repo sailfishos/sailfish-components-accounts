517be53e62c9b16e0b401b4f17f0db81e8cae6f2
Chris Adams <chris.adams@jollamobile.com>
2015-10-13 18:37:03 +1000

    [sailfish-components-accounts] Add support for remote cloud storage directory listing requests. Contributes to JB#33109

--- src/lib/cloudbackupsynctrigger.h
+++ src/lib/cloudbackupsynctrigger.h
@@ -12,6 +12,11 @@
 
 #include <QObject>
 #include <QString>
+#include <QNetworkAccessManager>
+#include <QVariantList>
+
+#include <SignOn/SessionData>
+#include <SignOn/Error>
 
 class Q_DECL_EXPORT CloudBackupSyncTrigger : public QObject
 {
@@ -20,25 +25,33 @@ class Q_DECL_EXPORT CloudBackupSyncTrigger : public QObject
 private:
     QString m_currentSyncProfileId;
     AccountSyncManager *m_accountSyncManager;
+    QNetworkAccessManager *m_networkManager;
 
 public:
     CloudBackupSyncTrigger(QObject *parent = 0);
 
     /* localDir and remotePath are optional.  The default localDir is /home/nemo/.local/share/system/privileged/Backups/ */
-    Q_INVOKABLE bool downloadFromCloud(int accountId, const QString &localDir = QString(), const QString &remotePath = QString());
+    Q_INVOKABLE bool downloadFromCloud(int accountId, const QString &localDir = QString(), const QString &remotePath = QString(), const QString &remoteFile = QString());
     Q_INVOKABLE bool uploadToCloud(int accountId, const QString &localDir = QString(), const QString &remotePath = QString());
+    Q_INVOKABLE bool requestListing(int accountId, const QString &remotePath = QString());
 
     /* no signals for the previous operation will be emitted after calling this, but you can immediately start a new operation. */
     Q_INVOKABLE void resetState();
 
 Q_SIGNALS:
     void cloudSyncProgress(int status, const QString &errorString);
+    void requestedListing(const QVariantList &listing);
+    void requestListingFailed(const QString &message);
 
 private:
-    bool syncWithCloud(int accountId, const QString &localDir, const QString &remotePath, const QString &direction);
+    bool syncWithCloud(int accountId, const QString &localDir, const QString &remotePath, const QString &remoteFile, const QString &direction);
+    void performListingRequest(int accountId, const QString &accessToken, const QString &cloudName, const QString &remotePath);
 
 private Q_SLOTS:
     void handleProfileSyncStatusChanged(const QString &profileId, int status, const QString &errorString);
+    void signOnResponse(const SignOn::SessionData &responseData);
+    void signOnError(const SignOn::Error &error);
+    void handleListingResponse();
 };
 
 #endif // CLOUDBACKUPSYNCTRIGGER_H
