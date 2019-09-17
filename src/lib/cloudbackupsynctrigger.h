ef405ee7394983e852ba0e2f9334f19002faea59
Bea Lam <bea.lam@jollamobile.com>
2019-09-17 11:33:19 +1000

    [accounts] Backup/restore: use sync plugin to do dir listings. Contributes to JB#47080
    CloudBackupSyncTrigger no longer performs the remote directory
    listing. It will ask the sync plugin to do this instead, by setting
    a dconf key that sets a 'list' operation before triggering the
    sync. If downloading/uploading a file for backup/restore, it sets
    a 'sync' operation instead.

--- src/lib/cloudbackupsynctrigger.h
+++ src/lib/cloudbackupsynctrigger.h
@@ -11,26 +11,13 @@
 #include "accountsyncmanager.h"
 
 #include <QObject>
-#include <QString>
-#include <QNetworkAccessManager>
-#include <QVariantList>
 #include <QStringList>
-
-#include <SignOn/SessionData>
-#include <SignOn/Error>
+#include <QNetworkAccessManager>
 
 class Q_DECL_EXPORT CloudBackupSyncTrigger : public QObject
 {
     Q_OBJECT
     Q_PROPERTY(QString defaultRemoteBackupsDirectory READ defaultRemoteBackupsDirectory CONSTANT)
-
-private:
-    QString m_currentSyncProfileId;
-    QString m_defaultRemoteBackupsDirectory;
-    QStringList m_deviceDirectories;
-    QVariantList m_dirListing;
-    AccountSyncManager *m_accountSyncManager;
-    QNetworkAccessManager *m_networkManager;
 
 public:
     CloudBackupSyncTrigger(QObject *parent = 0);
@@ -38,7 +25,7 @@ public:
     /* localDir and remotePath are optional.  The default localDir is /home/nemo/.local/share/system/privileged/Backups/ */
     Q_INVOKABLE bool downloadFromCloud(int accountId, const QString &localDir = QString(), const QString &remotePath = QString(), const QString &remoteFile = QString());
     Q_INVOKABLE bool uploadToCloud(int accountId, const QString &localDir = QString(), const QString &remotePath = QString());
-    Q_INVOKABLE bool requestListing(int accountId, const QString &remotePath = QString());
+    Q_INVOKABLE bool requestListing(int accountId);
 
     /* no signals for the previous operation will be emitted after calling this, but you can immediately start a new operation. */
     Q_INVOKABLE void resetState();
@@ -52,14 +39,21 @@ Q_SIGNALS:
     void requestListingFailed(int accountId, const QString &message);
 
 private:
+    bool initOperation(int accountId, const QString &operation);
     bool syncWithCloud(int accountId, const QString &localDir, const QString &remotePath, const QString &remoteFile, const QString &direction);
     void performListingRequest(int accountId, const QString &accessToken, const QString &cloudName, const QString &remotePath);
-
-private Q_SLOTS:
+    void processDirectoryListResults();
     void handleProfileSyncStatusChanged(const QString &profileId, int status, const QString &errorString);
-    void signOnResponse(const SignOn::SessionData &responseData);
-    void signOnError(const SignOn::Error &error);
-    void handleListingResponse();
+
+    int m_accountId = -1;
+    QString m_cloudServiceName;
+    QString m_currentOperation;
+    QString m_currentSyncProfileId;
+    QString m_defaultRemoteBackupsDirectory;
+    QStringList m_deviceDirectories;
+    QVariantList m_dirListing;
+    AccountSyncManager *m_accountSyncManager = nullptr;
+    QNetworkAccessManager *m_networkManager = nullptr;
 };
 
 #endif // CLOUDBACKUPSYNCTRIGGER_H
