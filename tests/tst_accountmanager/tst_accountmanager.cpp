7d806244fd443066dd31e18f7137bb1cad391dfe
Pekka Vuorela <pekka.vuorela@jolla.com>
2020-01-17 14:44:00 +0200

    Remove Qt4 qtest compatibility declarations

--- tests/tst_accountmanager/tst_accountmanager.cpp
+++ tests/tst_accountmanager/tst_accountmanager.cpp
@@ -19,38 +19,6 @@
 #include <Accounts/Manager>
 #include <Accounts/Account>
 
-// Will try to wait for the condition while allowing event processing
-#ifndef QTRY_VERIFY
-#define QTRY_VERIFY(__expr) \
-    do { \
-        const int __step = 50; \
-        const int __timeout = 5000; \
-        if (!(__expr)) { \
-            QTest::qWait(0); \
-        } \
-        for (int __i = 0; __i < __timeout && !(__expr); __i+=__step) { \
-            QTest::qWait(__step); \
-        } \
-        QVERIFY(__expr); \
-    } while (0)
-#endif
-
-// Will try to wait for the condition while allowing event processing
-#ifndef QTRY_COMPARE
-#define QTRY_COMPARE(__expr, __expected) \
-    do { \
-        const int __step = 50; \
-        const int __timeout = 5000; \
-        if ((__expr) != (__expected)) { \
-            QTest::qWait(0); \
-        } \
-        for (int __i = 0; __i < __timeout && ((__expr) != (__expected)); __i+=__step) { \
-            QTest::qWait(__step); \
-        } \
-        QCOMPARE(__expr, __expected); \
-    } while (0)
-#endif
-
 class tst_AccountManager : public QObject
 {
     Q_OBJECT
